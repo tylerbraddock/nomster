504704d125ad69c3ba45dffd185f6f99dda9f3e9
Tyler Braddock <tyler.braddock@yahoo.com>
2017-01-31 17:38:14 +0000

    Edited user permissions for destroy action in places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 10)
@@ -40,6 +40,11 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end
